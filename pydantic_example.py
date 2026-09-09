7c35b3b63726a65a0feb517bc5aec5da83fe3720
Ruslan-Lukyanets <ruslanlukyanets@yandex.ru>
2026-09-09 17:14:13 +0300

    Знакомство с Pydantic

---
+++ pydantic_example.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+    city: str
+    zip_code: str
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+    is_active: bool = True  # Значение по умолчанию
+    address: Address  # Вложенная модель
+
+user = User(id=1, name="Alice", address={"city": "New York", "zip_code": "10001"})
+print(user.address.city)  # "New York"
